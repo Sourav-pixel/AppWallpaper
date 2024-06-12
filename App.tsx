77a2459e647fb5c93979ddd2931530841bda4a73
Sourav Deka <84978182+Sourav-pixel@users.noreply.github.com>
2024-06-12 16:24:25 +0530

    "Updated App.tsx with new functionality, modified android build configuration, and added dependencies"

--- App.tsx
+++ App.tsx
@@ -1,117 +1,221 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  FlatList,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  Alert,
+  ActivityIndicator,
+  ScrollView,
 } from 'react-native';
+import axios from 'axios';
+import RNFS from 'react-native-fs';
+import CustomCarousel from './Carousel';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const {width} = Dimensions.get('window');
+const numColumns = 2;
+const tileSize = width / numColumns - 10;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [filteredImages, setFilteredImages] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  useEffect(() => {
+    fetchImages();
+  }, []);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const fetchImages = async () => {
+    try {
+      const response = await axios.get(
+        'https://wallpaper-0jy8.onrender.com/images',
+      );
+      const images = response.data;
+      setImages(images);
+      setFilteredImages(images);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+      const categorySet = new Set(images.map(image => image.category));
+      setCategories(Array.from(categorySet));
+
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching images:', error);
+      setIsLoading(false);
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+  const downloadImage = async (url, filename) => {
+    try {
+      const downloadDest = `${RNFS.DownloadDirectoryPath}/${filename}`;
+      const ret = RNFS.downloadFile({
+        fromUrl: url,
+        toFile: downloadDest,
+      });
+
+      await ret.promise;
+      Alert.alert('Download Complete', `Image downloaded to ${downloadDest}`);
+    } catch (error) {
+      console.error('Error downloading image:', error);
+      Alert.alert('Error', 'Error downloading image');
+    }
+  };
+
+  const applyCategoryFilter = category => {
+    if (category === selectedCategory) {
+      setSelectedCategory('');
+      setFilteredImages(images);
+    } else {
+      const filtered = images.filter(image => image.category === category);
+      setSelectedCategory(category);
+      setFilteredImages(filtered);
+    }
+  };
+
+  const renderImageTile = ({item}) => (
+    <View style={styles.tile}>
+      <Image
+        source={{uri: `https://wallpaper-0jy8.onrender.com${item.url}`}}
+        style={styles.image}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.category}>{item.category}</Text>
+      <TouchableOpacity
+        style={styles.downloadButton}
+        onPress={() =>
+          downloadImage(
+            `https://wallpaper-0jy8.onrender.com${item.url}`,
+            `${item.title}.jpg`,
+          )
+        }>
+        <Text style={styles.downloadButtonText}>Download</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator size="large" color="#007BFF" />
         </View>
-      </ScrollView>
+      ) : (
+        <View style={styles.contentContainer}>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={styles.filterContainer}>
+            <TouchableOpacity
+              style={[
+                styles.filterButton,
+                selectedCategory === '' && styles.activeFilter,
+              ]}
+              onPress={() => applyCategoryFilter('')}>
+              <Text style={styles.filterButtonText}>All</Text>
+            </TouchableOpacity>
+            {categories.map(category => (
+              <TouchableOpacity
+                key={category}
+                style={[
+                  styles.filterButton,
+                  selectedCategory === category && styles.activeFilter,
+                ]}
+                onPress={() => applyCategoryFilter(category)}>
+                <Text style={styles.filterButtonText}>{category}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+          <FlatList
+            data={filteredImages}
+            renderItem={renderImageTile}
+            keyExtractor={item => item._id}
+            numColumns={numColumns}
+            contentContainerStyle={styles.list}
+          />
+        </View>
+      )}
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
+  loaderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 5,
+  },
+  list: {
+    padding: 5,
+  },
+  tile: {
+    flex: 1,
+    margin: 5,
+    alignItems: 'center',
+    borderRadius: 10,
+    overflow: 'hidden',
+    backgroundColor: '#f8f8f8',
+    elevation: 2,
+  },
+  image: {
+    width: tileSize,
+    height: 400,
+  },
+  title: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: 'bold',
+    marginVertical: 5,
+    textAlign: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  category: {
+    marginBottom: 10,
+    textAlign: 'center',
+    height: 30,
   },
-  highlight: {
-    fontWeight: '700',
+  downloadButton: {
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: '#007BFF',
+    borderRadius: 10,
+  },
+  downloadButtonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+    marginTop: 10,
+    height: 70, // Fixed height for category buttons
+  },
+  filterButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#ccc',
+    borderRadius: 20,
+    marginHorizontal: 5,
+    height: 40, // Fixed height for category buttons
+  },
+  filterButtonText: {
+    fontWeight: 'bold',
+  },
+  activeFilter: {
+    backgroundColor: '#007BFF',
+    color: '#fff',
   },
 });
 
